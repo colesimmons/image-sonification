c802af37d0cebc5bbcd7b3f2b93cb7e3ec4b3c49
Cole Simmons <colesimmons223@gmail.com>
2022-12-03 18:53:58 -0800

    Chuck status is...in progress. Moving on to photos.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 
 export default function Home() {
+  const play = async () => {
+    /*
+    const Chuck = (await import('webchuck')).Chuck;
+    const chuck = await Chuck.init([], "./chuck-processor.js");
+    if (!chuck) return
+    const x = chuck.runCode(
+      `
+      SinOsc sin => dac;
+      sin.freq(220);
+      1000::ms => now;
+      `
+    );
+    console.log(x);
+     */
+  }
+
   return (
     <div>
       <Head>
@@ -9,6 +26,7 @@ export default function Home() {
         <meta name="description" content="music has never looked so good" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <button onClick={play}>Play</button>
       <p>Hello World</p>
     </div>
   )
